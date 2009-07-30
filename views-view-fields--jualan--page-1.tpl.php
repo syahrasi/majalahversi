8b646efea7ece70953ec44172c3e47b876ee42cc
Syahrasi <syahrasi@gmail.com>
2009-07-30 17:16:37 +0800

    re: #28 styling node list u/ page-iklan

--- views-view-fields--jualan--page-1.tpl.php
+++ views-view-fields--jualan--page-1.tpl.php
@@ -1,15 +1,28 @@
+<div class='row-date'>
+<?php 
+  if ($fields['field_status_value']->content) {
+    print $fields['created']->content;
+  } else {
+    print 'CLOSED';    
+  }
+?>
+</div>
 <div class='row-container'>
   <div class='row-thumb'>
     <?php print $fields['field_main_image_fid']->content; ?>    
   </div>
+  <p class='row-info'>
+    <?php if($fields['name']->content): ?>
+      <span class='row-term'>
+        <?php print $fields['name']->content; ?>
+      </span>
+    <?php endif ?>
+  </p>
   <h3 class='row-title'>
     <?php print $fields['title']->content; ?>      
   </h3>
-  <p>
-    <span class='row-term'><?php print $fields['name']->content; ?></span>,&nbsp;&nbsp;
-    <?php print $fields['teaser']->content; ?>
-  </p>
+  <div class='row-teaser'>
+    <?php print $fields['teaser']->content; ?><div class='read-more'><?php print $fields['view_node']->content; ?></div>
+  </div>
 </div>
-<div class='row-date'>
-    <?php  print $fields['created']->content; ?>
-</div>
+
