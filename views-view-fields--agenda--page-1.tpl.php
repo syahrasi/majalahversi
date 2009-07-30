d95c99173c0437f8d92ddfecfe1a3c684a98274b
Syahrasi <syahrasi@gmail.com>
2009-07-30 11:32:13 +0800

    re: #26 edit views template untuk page agenda

--- views-view-fields--agenda--page-1.tpl.php
+++ views-view-fields--agenda--page-1.tpl.php
@@ -3,13 +3,20 @@
     <?php print $fields['field_main_image_fid']->content; ?>    
   </div>
   <p class='row-info'>
-    <span class='row-term'><?php print $fields['name']->content; ?></span>
-      <h3 class='row-title'>
-        <?php print $fields['title']->content; ?>      
-      </h3>
-    <span class='row-date'><?php print $fields['field_date_value']->content; ?></span>,&nbsp;&nbsp;
-    <span class='row-location'><?php print $fields['field_location_value']->content; ?></span>,&nbsp;&nbsp;
-    <span class='row-city'><?php print $fields['field_city_value']->content; ?></span>
-    <?php print $fields['teaser']->content; ?>
+    <?php if($fields['tid']->content): ?>
+      <span class='row-term'>
+        <?php print $fields['tid']->content; ?>
+      </span>
+    <?php endif ?>
+    <span class='row-date'><?php print $fields['created']->content; ?></span>
   </p>
+  <h3 class='row-title'>
+    <?php print $fields['title']->content; ?>      
+  </h3>
+  <span class='row-date'><?php print $fields['field_date_value']->content; ?></span>,&nbsp;&nbsp;
+  <span class='row-location'><?php print $fields['field_location_value']->content; ?></span>,&nbsp;&nbsp;
+  <span class='row-city'><?php print $fields['field_city_value']->content; ?></span>
+  <div class='row-teaser'>
+    <?php print $fields['teaser']->content; ?><div class='read-more'><?php print $fields['view_node']->content; ?></div>
+  </div>
 </div>
