233510a992ea4fc7f26558176c048ee4277c0650
Syahrasi <syahrasi@gmail.com>
2009-07-30 16:38:44 +0800

    kurangi spasi antar span di view-lowongan

--- views-view-fields--lowongan--page-2.tpl.php
+++ views-view-fields--lowongan--page-2.tpl.php
@@ -12,8 +12,8 @@
     <?php print $fields['title']->content; ?>      
   </h3>
   <p>
-    <span class='row-company'><?php print $fields['field_company_value']->content; ?></span>,&nbsp;&nbsp;
-    <span class='row-term'><?php print $fields['name']->content; ?></span>,&nbsp;&nbsp;
+    <span class='row-company'><?php print $fields['field_company_value']->content; ?></span>,&nbsp;
+    <span class='row-term'><?php print $fields['name']->content; ?></span>,&nbsp;
     <span class='row-city'><?php print $fields['field_city_value']->content; ?></span>
   </p>
 </div>
