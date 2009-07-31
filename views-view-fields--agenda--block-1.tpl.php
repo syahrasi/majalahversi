5436aff5ac19afbca5852350c5316de97120975a
Syahrasi <syahrasi@gmail.com>
2009-07-31 08:12:00 +0800

    re: #29 adding agenda terbaru block to sidebar

--- views-view-fields--agenda--block-1.tpl.php
+++ views-view-fields--agenda--block-1.tpl.php
@@ -1,1 +1,5 @@
+<?php print $fields['field_date_value']->content; ?><br />
+<?php print $fields['field_location_value']->content; ?><br />
+<?php print $fields['field_city_value']->content; ?><br />
 <?php print $fields['title']->content; ?>
+
