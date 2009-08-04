50d49e35493119e3221a0a830bf2c28257bf5477
Syahrasi <syahrasi@gmail.com>
2009-08-04 11:28:59 +0800

    re: #23 adding category for image thumbnail in page-galeri

--- views-view-fields--galeri--page.tpl.php
+++ views-view-fields--galeri--page.tpl.php
@@ -1,4 +1,7 @@
 <div class='row-container'>
+  <div class='row-term'>
+    <?php print $fields['name']->content; ?>    
+  </div>
   <div class='row-image'>
     <?php print $fields['field_main_image_fid']->content; ?>    
   </div>
