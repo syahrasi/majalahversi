fec0e5f115b55b2976009b0305de0f4dbe0ff20d
Syahrasi <syahrasi@gmail.com>
2009-08-09 20:40:23 +0800

    fixing #54 and improving galeri thumbnail styling

--- views-view-fields--galeri--page.tpl.php
+++ views-view-fields--galeri--page.tpl.php
@@ -8,7 +8,7 @@
     <?php print $fields['field_main_image_fid']->content; ?>    
   </div>
   <div class='row-galeri-info'>
-    <p><?php print $fields['title']->content; ?></p>
+    <div class='row-title'><?php print $fields['title']->content; ?></div>
     <div class='row-rating'><?php print $fields['value']->content; ?></div>
     <div class='row-comment-count'><?php print $fields['comment_count']->content; ?></div>
   </div>
