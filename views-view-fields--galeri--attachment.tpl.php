81c1cb7ad00a2525573d1e96569f6631d778d417
Syahrasi <syahrasi@gmail.com>
2009-08-12 12:39:12 +0800

    wrapping $fields with if statements for more adaptability

--- views-view-fields--galeri--attachment.tpl.php
+++ views-view-fields--galeri--attachment.tpl.php
@@ -1,14 +1,23 @@
 <div class='row-container'>
-  <div class='row-term'>
-    <?php print $fields['name']->content; ?>    
-  </div>
-  <div class='row-image'>
-    <?php print $fields['field_main_image_fid']->content; ?>    
-  </div>
-  <div class='row-galeri-info'>
-    <p><?php print $fields['title']->content; ?></p>
-    <div class='row-comment-count'><?php print $fields['comment_count']->content; ?></div>
-    <div class='row-rating'><?php print $fields['value']->content; ?></div>
-  </div>
+  <?php if($fields['name']->content): ?>
+    <div class='row-term'>
+      <?php print $fields['name']->content; ?>    
+    </div>
+  <?php endif ?>  
+  <?php if($fields['field_main_image_fid']->content): ?>
+    <div class='row-image'>
+      <?php print $fields['field_main_image_fid']->content; ?>    
+    </div>
+  <?php endif ?>
+  <?php if($fields['title']->content): ?>
+    <div class='row-galeri-info'>
+      <p><?php print $fields['title']->content; ?></p>
+      <?php if($fields['comment_count']->content): ?>
+          <div class='row-comment-count'><?php print $fields['comment_count']->content; ?></div>
+      <?php endif ?>
+      <?php if($fields['value']->content): ?>
+          <div class='row-rating'><?php print $fields['value']->content; ?></div>
+      <?php endif ?>
+    </div>  
+  <?php endif ?>
 </div>
-
