6d9cf840692dc3b95beee8c093daf14198cb88e5
Syahrasi <syahrasi@gmail.com>
2009-08-15 14:34:43 +0800

    improving views template for latest-articles.tpl.php

--- views-view-fields--latest-articles.tpl.php
+++ views-view-fields--latest-articles.tpl.php
@@ -1,1 +1,7 @@
-<?php print $fields['title']->content; ?>   
+<?php if($fields['title']->content): ?>
+  <?php print $fields['title']->content; ?>
+<?php endif ?>
+<?php if($fields['field_name_value']->content): ?>
+  &nbsp;&nbsp;-&nbsp;&nbsp;<?php print $fields['field_name_value']->content; ?>     
+<?php endif ?>
+
