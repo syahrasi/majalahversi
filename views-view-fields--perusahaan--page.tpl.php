5ec2cc53cae99884d5539f4e0ca27272b589c1ea
Syahrasi <syahrasi@gmail.com>
2009-08-16 16:37:06 +0800

    improving perusahaan page views template for more adaptability

--- views-view-fields--perusahaan--page.tpl.php
+++ views-view-fields--perusahaan--page.tpl.php
@@ -2,18 +2,30 @@
   <div class='row-thumb'>
     <?php print $fields['field_main_image_fid']->content; ?>    
   </div>
-  <p class='row-info'>
-    <?php if($fields['name']->content): ?>
+  <?php if($fields['name']->content || $fields['field_city_value']->content): ?>
+    <p class='row-info'>
       <span class='row-term'>
         <?php print $fields['name']->content; ?>
       </span>
-    <?php endif ?>
-    <span class='row-date'><?php print $fields['created']->content; ?></span>
-  </p>
-  <h2 class='row-title'>
-    <?php print $fields['title']->content; ?>      
-  </h2>
+      <?php if($fields['name']->content && $fields['field_city_value']->content): ?>
+        &nbsp;&nbsp;&raquo;&nbsp;&nbsp;
+      <?php endif ?>
+      <span class='row-city'>
+        <?php print $fields['field_city_value']->content; ?>
+      </span>
+    </p>
+  <?php endif ?>
+  <?php if($fields['title']->content): ?>
+    <h2 class='row-title'>
+      <?php print $fields['title']->content; ?>      
+    </h2>
+  <?php endif ?>
+  <?php if($fields['teaser']->content): ?>
   <div class='row-teaser'>
-    <?php print $fields['teaser']->content; ?><div class='read-more'><?php print $fields['view_node']->content; ?></div>
+    <?php print $fields['teaser']->content; ?>
+    <div class='read-more'>
+      <?php print $fields['view_node']->content; ?>
+    </div>
   </div>
+  <?php endif ?>
 </div>
