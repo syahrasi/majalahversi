188edf28d6ddfbecab13ebe522319d56d9a96b0b
Syahrasi <syahrasi@gmail.com>
2009-08-29 16:00:15 +0800

    add views template for komentar terbaru blocks and capitalize comment author
    add views template for komentar terbaru blocks
    
    add styling for comment author  in  komentar terbaru blocks

---
+++ views-view-fields--comments-recent--block.tpl.php
@@ -0,0 +1,3 @@
+<span class="comment-author"><?php print $fields['name']->content; ?></span>&#58;&nbsp;
+<?php print $fields['timestamp']->content; ?><br/>
+<?php print $fields['subject']->content; ?>&mdash;<?php print $fields['title']->content; ?>
