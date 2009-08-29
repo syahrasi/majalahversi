32db05086f76242a9c9cd119ae6f26717f3d902c
Syahrasi <syahrasi@gmail.com>
2009-08-29 15:48:23 +0800

    adjusting selectors and adding fields in latest articles block  views for future use

--- views-view-fields--latest-articles--block.tpl.php
+++ views-view-fields--latest-articles--block.tpl.php
@@ -1,6 +1,9 @@
 <?php if($fields['title']->content): ?>
   <?php print $fields['title']->content; ?>
 <?php endif ?>
+<?php if($fields['comment_count']->content): ?>
+  <span class="count-number"><?php print $fields['comment_count']->content; ?></span>
+<?php endif ?>
 <?php if($fields['field_name_value']->content): ?>
   &nbsp;&nbsp;-&nbsp;&nbsp;<span class='sdp-pengirim'><?php print $fields['field_name_value']->content; ?></span>
 <?php endif ?>
