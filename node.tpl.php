fbbc2a06be4f55b04f6c971aa74bbfdc404d33ab
Syahrasi <syahrasi@gmail.com>
2009-09-08 13:28:38 +0800

    adding conditional statement to hide title when viewing node suara

--- node.tpl.php
+++ node.tpl.php
@@ -1,13 +1,6 @@
 
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
 
-<?php if ($page == 0): ?>
-  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-<?php else: ?>
-  <?php if($node->nid != 1374): ?>
-    <h2 class="node-title"><?php print $title ?></h2>    
-  <?php endif ?>
-<?php endif; ?>
 <?php if ($date || $authors): ?>
   <div class="node-submit-info">
   <?php if ($authors): ?>
@@ -26,6 +19,13 @@
     <?php print $terms ?>
   <?php endif; ?>
   </div>
+<?php endif; ?>
+<?php if ($page == 0): ?>
+  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+<?php else: ?>
+  <?php if($node->nid != 1374): ?>
+    <h2 class="node-title"><?php print $title ?></h2>    
+  <?php endif ?>
 <?php endif; ?>
 <?php if ($submitted): ?>
   <div class="meta">
